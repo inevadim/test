5862812e75de881bfb9c56fb8a85a69a7c4b46a3
inevadim <kvadrocheyk@gmail.com>
2024-02-10 14:58:01 +0300

    structure FeedBackForm

--- src/components/feedBackForm/FeedBackForm.jsx
+++ src/components/feedBackForm/FeedBackForm.jsx
@@ -1,5 +1,10 @@
+import style from './FeedBackForm.module.scss'; 
+
 export const FeedBackForm=()=>{
-    return(<div>
-        FeedBackForm
+    return(<div className={style.wrapper}>
+        <div className={style.wrapperItem}>Name: <input type='text'></input></div>
+        <div className={style.wrapperItem}>E-mail <input type='email'></input></div>
+        <div className={style.wrapperItem}>Phone <input type='tel'></input></div>
+        <div className={style.wrapperItem}>Message <textarea></textarea></div>
     </div>)
 }
