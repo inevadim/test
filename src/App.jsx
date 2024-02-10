566d77f8c77ed9a2f8993913fed92e3d1fe7d521
inevadim <kvadrocheyk@gmail.com>
2024-02-10 14:50:10 +0300

    create component FeedBackForm and css module

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { FeedBackForm } from "./components/feedBackForm/FeedBackForm";
+
+function App() {
+  return (
+    <div className="App">
+      <FeedBackForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/feedBackForm/FeedBackForm.jsx
@@ -0,0 +1,5 @@
+export const FeedBackForm=()=>{
+    return(<div>
+        FeedBackForm
+    </div>)
+}
