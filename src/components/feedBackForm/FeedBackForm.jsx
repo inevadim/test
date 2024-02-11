70486d314f5d11be240110404f9a5ec95b25b12c
inevadim <kvadrocheyk@gmail.com>
2024-02-11 14:20:56 +0300

    validation phone number and add dependencies react-phone-input-2

--- src/components/feedBackForm/FeedBackForm.jsx
+++ src/components/feedBackForm/FeedBackForm.jsx
@@ -5,6 +5,8 @@ import { faPhone } from '@fortawesome/free-solid-svg-icons';
 import { faComments } from '@fortawesome/free-solid-svg-icons';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import { PhoneNumberValidation } from './validation/PhoneNumberValidation';
+
 
 
 export const FeedBackForm=()=>{
@@ -12,7 +14,8 @@ export const FeedBackForm=()=>{
         <div className={style.FeedBackForm}>Feed back form</div>
         <div className={style.wrapperItem}><div><FontAwesomeIcon icon={faUser} /></div><input type='text' placeholder='Name'></input></div>
         <div className={style.wrapperItem}><FontAwesomeIcon icon={faAt} /><input type='email' placeholder='email'></input></div>
-        <div className={style.wrapperItem}> <FontAwesomeIcon icon={faPhone} /><input type='tel' placeholder='phone'></input></div>
+        {/* <div className={style.wrapperItem}> <FontAwesomeIcon icon={faPhone} /><input type='tel' placeholder='phone'></input></div> */}
+        <div className={style.wrapperItem}><FontAwesomeIcon icon={faPhone} /><PhoneNumberValidation></PhoneNumberValidation></div>
         <div className={style.wrapperItem}><FontAwesomeIcon icon={faComments} /> <textarea placeholder='Message' color='white' cols="21" rows="6" ></textarea></div>
         <div className={style.wrapperItem}> <div className={style.send}><FontAwesomeIcon icon={faPaperPlane} /> Send</div></div>
     </div>)
---
+++ src/components/feedBackForm/validation/PhoneNumberValidation.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import PhoneInput from 'react-phone-input-2';
+import 'react-phone-input-2/lib/style.css';
+
+export const PhoneNumberValidation = () => {
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [valid, setValid] = useState(true);
+
+  const handleChange = (value) => {
+    setPhoneNumber(value);
+    setValid(validatePhoneNumber(value));
+  };
+
+  const validatePhoneNumber = (phoneNumber) => {
+    const phoneNumberPattern = /^\+?[1-9]\d{1,14}$/;
+
+    return phoneNumberPattern.test(phoneNumber);
+  };
+
+  return (
+    <div>
+      <label>
+        Phone Number:
+        <PhoneInput
+          country={'by'}
+          value={phoneNumber}
+          onChange={handleChange}
+          inputProps={{
+            required: true,
+          }}
+        />
+      </label>
+      {!valid && (
+        <p>Please enter a valid phone number.</p>
+      )}
+    </div>
+  );
+};
+
